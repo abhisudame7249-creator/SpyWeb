7c3ffcd7169030521fa52670e0c60889993e8401
Abhishek Sudame <abhisudame7249@gmail.com>
2026-02-19 19:46:28 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,17 @@
 require("dotenv").config();
 
-process.on("uncaughtException", err => {
-  console.error("UNCAUGHT:", err);
+process.on("uncaughtException", (err) => {
+  console.error("UNCAUGHT EXCEPTION:", err);
 });
-process.on("unhandledRejection", err => {
-  console.error("UNHANDLED:", err);
+
+process.on("unhandledRejection", (err) => {
+  console.error("UNHANDLED REJECTION:", err);
 });
 
 const express = require("express");
 const cors = require("cors");
-const connectDB = require("./config/db");
 
 const app = express();
-
-connectDB();
 
 app.use(cors());
 app.use(express.json());
@@ -23,6 +21,7 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log("Server started on port", PORT);
 });
